93dc263124e24fe5c19858d837664ed38fb39fb2
Ash-water <mizuwater78@gmail.com>
2021-01-21 20:40:07 +0900

    テストコード修正1
    Factorybotのbuildの記述を一つにまとめてbefore  doでまとめた
    さらに正常形のテストをひとつ追加した

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   # 新規登録/ユーザー情報バリデーション
   validates :nickname, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'パスワードには英字と数字の両方を含めて設定してください'
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角英字数字の混合で入力してください'
 
   # 新規登録/本人情報確認
   with_options presence: true do
