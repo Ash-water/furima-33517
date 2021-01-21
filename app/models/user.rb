85befc5115f55ee18ee9a0bf8147e5b24fbc00c9
Ash-water <mizuwater78@gmail.com>
2021-01-21 15:59:18 +0900

    userモデルにバリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # 新規登録/ユーザー情報バリデーション
+  validates :nickname, presence: true
+  validates :email, uniqueness: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'パスワードには英字と数字の両方を含めて設定してください'  
+  
+  # 新規登録/本人情報確認
+  with_options presence: true do
+    validates :last_name
+    validates :first_name
+    validates :last_name
+    validates :furi_first
+    validates :furi_last
+    validates :date_of_birth
+  end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+
 end
