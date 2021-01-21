729c0a3e7a1b68dc6139e5ea31fee21da4e63873
Ash-water <mizuwater78@gmail.com>
2021-01-21 16:29:17 +0900

    userモデルテストコードの雛形
    雛形を記述しfactory_botも導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   # 新規登録/ユーザー情報バリデーション
   validates :nickname, presence: true
-  validates :email, uniqueness: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'パスワードには英字と数字の両方を含めて設定してください'  
-  
+
   # 新規登録/本人情報確認
   with_options presence: true do
     validates :last_name
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "ユーザー新規登録" do
+    it "ニックネームが必須であること" do
+      user = User.new(nickname: "", email: "test@example", password: "000000", password_confirmation: "000000")
+      user.valid?
+      expect(user.errors.full_messages).to include("")
+    end
+    it "メールアドレスが必須であること" do
+      
+    end
+    it "メールアドレスが一意性であること" do
+      
+    end
+    it "メールアドレスは、@を含む必要があること" do
+      
+    end
+    it "パスワードが必須であること" do
+      
+    end
+    it "パスワードは、6文字以上での入力が必須であること" do
+      
+    end
+    it "パスワードは、半角英数字混合での入力が必須であること" do
+      
+    end
+    it "パスワードが存在してもパスワード確認が空では登録できないこと" do
+      
+    end
+    it "パスワードとパスワード（確認用）、値の一致が必須であること" do
+      
+    end
+    it "ユーザー本名は、名字と名前がそれぞれ必須であること" do
+      
+    end
+    it "ユーザー本名は、全角（漢字・ひらがな・カタカナ）での入力が必須であること" do
+      
+    end
+    it "ユーザー本名のフリガナは、名字と名前でそれぞれ必須であること" do
+      
+    end
+    it "ユーザー本名のフリガナは、全角（カタカナ）での入力が必須であること" do
+      
+    end
+    it "生年月日が必須であること" do
+      
+    end
+    
+  end
 end
