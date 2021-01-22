07037f09b401f1cffb70505bd133d03455e7e400
Ash-water <mizuwater78@gmail.com>
2021-01-22 14:01:51 +0900

    itemsテーブルの作成

--- db/migrate/20210122042118_create_items.rb
+++ db/migrate/20210122042118_create_items.rb
@@ -1,8 +1,16 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :name, null: false
+      t.integer :price, null: false
+      t.text :item_description, null: false
+      t.integer :category_id, null: false
+      t.integer :quality_id, null: false
+      t.integer :delivery_fee_id, null: false
+      t.integer :shipping_date_id, null: false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
 end
+
