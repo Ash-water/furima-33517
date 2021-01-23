547ac755f3e8458bced1f78fb77dd299e62a2ebb
Ash-water <mizuwater78@gmail.com>
2021-01-23 11:58:15 +0900

    itemsモデルのバリデーション
    バリデーションの記述と
    active Hashのアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,23 @@
 class Item < ApplicationRecord
+  #Assosiations
   belongs_to :user
   has_one_attached :image
+  #Assosiations with ActiveHash
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :prefecture, :delivery_fee, :quality, :shipping_date,
+
+  #validations
+  with_options presence: true do
+    validates :name
+    validates :price
+    validates :item_description
+    validates :category_id
+    validates :quality_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :shipping_data_id
+    validates :user
+  end
+
+  
 end
