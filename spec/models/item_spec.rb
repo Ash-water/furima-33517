bad8c7a52811c8672f519f8005ebf20ead5ed886
Ash-water <mizuwater78@gmail.com>
2021-01-26 13:58:04 +0900

    正常系テストを記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品出品機能' do
+
+    context '商品が出品できる時' do
+      it '必須情報が全て適切に入力されている' do
+      @item = FactoryBot.build(:item)
+      expect(@item).to be_valid
+      end
+    end
+  
+  end
 end
+
+# RSpec.describe User, type: :model do
+#   before do
+#     @user = FactoryBot.build(:user)
+#   end
+
+#   describe 'ユーザー新規登録' do
+
+#     context '新規登録できる時' do
+#       it '全ての入力項目が適切に入力されている' do
+#         expect(@user).to be_valid
+#       end
+#     end
