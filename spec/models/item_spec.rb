3cb5af8b19c2f2cca4bfa32e3e59830a96d4240a
Ash-water <mizuwater78@gmail.com>
2021-01-26 14:04:20 +0900

    異常系のコードの名称を記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,27 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  before do
+    @item = FactoryBot.build(:item)
+  end
   describe '商品出品機能' do
 
     context '商品が出品できる時' do
       it '必須情報が全て適切に入力されている' do
-      @item = FactoryBot.build(:item)
       expect(@item).to be_valid
       end
     end
   
+    context '商品が出品できない時' do
+      it '商品画像が必須であること' do
+      end
+      it '商品名が必須であること' do
+      end
+      it '商品の説明が必須であること' do
+      end
+      it 'カテゴリーの情報が必須であること' do
+      end
+      it '商品の状態についての情報が必須' do
+      end
+      it '配送料の負担についての情報が必須であること' do
+      end
+      it '発送元の地域についての情報が必須であること' do
+      end
+      it '発送までの日数についての情報が必須であること' do
+      end
+      it '価格についての情報が必須であること' do
+      end
+      it '価格の範囲が¥300~¥9,999,999の間であること' do
+      end
+      it '販売価格は半角数字のみ保存可能であること' do
+      end
+    end
+
   end
 end
 
-# RSpec.describe User, type: :model do
-#   before do
-#     @user = FactoryBot.build(:user)
-#   end
-
-#   describe 'ユーザー新規登録' do
-
-#     context '新規登録できる時' do
-#       it '全ての入力項目が適切に入力されている' do
-#         expect(@user).to be_valid
-#       end
-#     end
