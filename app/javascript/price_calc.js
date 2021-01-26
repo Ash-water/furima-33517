da5ac60132d07e5f2157e1a927a4f49dba11472e
Ash-water <mizuwater78@gmail.com>
2021-01-26 16:12:29 +0900

    シンボルを用いた記述に変更

--- app/javascript/price_calc.js
+++ app/javascript/price_calc.js
@@ -1,9 +1,7 @@
 window.addEventListener('load', () => {
-  console.log('Hello You');
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () =>{
     const inputValue = priceInput.value;
-    console.log(inputValue)
     const addTaxDom = document.getElementById("add-tax-price");
     tax = inputValue * 0.1
     addTaxDom.innerHTML = Math.floor(tax)
