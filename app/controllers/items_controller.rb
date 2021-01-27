4f21bf2261b77d44a730cfe627674d9348ac4488
Ash-water <mizuwater78@gmail.com>
2021-01-27 20:40:52 +0900

    削除の条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,8 +36,10 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    item.destroy
+    if item.destroy
     redirect_to root_path
+    else
+    render "show"
   end
 
   private
