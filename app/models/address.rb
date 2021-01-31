76d9b5629689c09b047ca0adda6ae6f75ac773cb
Ash-water <mizuwater78@gmail.com>
2021-01-31 16:47:52 +0900

    値が必須なことのバリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,12 @@
 class Address < ApplicationRecord
   belongs_to :purchase
 
+  with_options presence: true do
+    validates :postal_code
+    velidates :prefecture_id
+    validates :city
+    validates :street
+    validates :phone_number
+    validates :purchase
+  end
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,4 +2,11 @@ class Purchase < ApplicationRecord
   has_one :address
   belongs_to :user
   belongs_to :item
+
+  with_options presence: true do
+    validates :user
+    validates :item
+  end
+
+  
 end
