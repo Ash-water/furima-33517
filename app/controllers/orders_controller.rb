f291b8e377dc9a2289935edd7bdfb95bb4de841b
Ash-water <mizuwater78@gmail.com>
2021-02-01 18:07:21 +0900

    フォームオブジェクト編集
    FOで別々のテーブルに情報を保存できるようにした

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,21 +2,23 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @purchase_form = PurchaseForm.new
   end
 
   def create
-    @purchase = PurchaseForm.new(purchase_form_params)
-    if @purchase.valid?
-      @purchase.save
+    @item = Item.find(params[:item_id])
+    @purchase_form = PurchaseForm.new(purchase_form_params)
+    if @purchase_form.valid?
+      @purchase_form.save
+      redirect_to root_path
     else
       render "orders#index"
     end
-
   end
 
   private
 
   def purchase_form_params
-    params.require(:purchase_form).parmit()
+    params.require(:purchase_form).permit(:postal_code, :prefecture_id, :street, :city, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -1,34 +1,40 @@
 class PurchaseForm
   
-  include ActiveModel::Active
+  include ActiveModel::Model
   #attr_accrsssorでここで取り扱う値を設定
-  attr_accessor :postal_code, :prefecture_id, :street, :building_name, :phone_number, :user, :item
-
+  attr_accessor :postal_code, :prefecture_id, :street, :city, :building_name, :phone_number, :user_id, :item_id
+  
   #addres validation
   with_options presence: true do
     validates :postal_code
-    velidates :prefecture_id
+    validates :prefecture_id
     validates :city
     validates :street
     validates :phone_number
-    validates :purchase
   end
 
   #purchase validation
   with_options presence: true do
-    validates :user
-    validates :item
+    validates :user_id
+    validates :item_id
   end
 
-  #card validation
-  with_options presence:true do
-    validates :number
-    validates :cvc
-    validates :exp_month
-    validates :exo_year
-  end
-  
+
   #saveメソッドを定義テーブルごとにcreateメソッドを適用させていくイメージ
+  def save
+    purchase = Purchase.create(
+      user_id: user_id,
+      item_id: item_id
+    )
 
-  
-
+    Address.create(
+      postal_code: postal_code,
+      prefecture_id: prefecture_id,
+      street: street,
+      city: city,
+      building_name: building_name,
+      phone_number: phone_number,
+      purchase_id: purchase.id #上記でpurchaseテーブルに保存したデータを変数purchaseに代入しそのIDを呼び出している
+    )
+  end
+end
