34269086de154f7734acf0bf6c5358e9b53f2db7
Ash-water <mizuwater78@gmail.com>
2021-02-02 15:04:11 +0900

    修正２

--- spec/models/purchase_form.rb
+++ spec/models/purchase_form.rb
@@ -4,7 +4,7 @@ RSpec.describe PurchaseForm, type: :model do
   before do
     @item = FactoryBot.create(:item) #itemをまず生成して、それに紐づくuser生成されます。それをしたの記述でmergeしてる感じ.fakerを使うことでユーザーの重複も解消できる
     @purchase_form = FactoryBot.build(:purchase_form, user_id:@item.user_id, item_id:@item.id)
-  sleep 1
+    sleep 1
   end
 
   describe '商品購入機能' do
