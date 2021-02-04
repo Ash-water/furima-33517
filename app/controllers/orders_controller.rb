107590e0d1ab4dc702d2e65e40280b8f8559a614
Ash-water <mizuwater78@gmail.com>
2021-02-04 19:36:44 +0900

    遷移修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,9 @@
 class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
   before_action :authenticate_user!, only: [:index, :create]
-  before_action :move_to_index, only:[:index, :create]
-
+  before_action :move_to_index, only: [:index, :create] 
+  before_action :seller_move_to_index, only: [:index, :create]
+  
   def index
     @purchase_form = PurchaseForm.new
   end
@@ -40,7 +41,13 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    if current_user.id == @item.user.id && @item.purchase != nil
+    if current_user.id != @item.user.id && @item.purchase != nil
+      redirect_to root_path
+    end
+  end
+
+  def seller_move_to_index
+    if current_user.id == @item.user.id
       redirect_to root_path
     end
   end
